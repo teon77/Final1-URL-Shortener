4323376af82670e623dbccab1bbd068a538d00ad
Eithan Refaely <eithanrefaely@gmail.com>
2021-11-03 19:25:46 +0200

    Finished Front setup
    Finished Front basic setup using bootstrap, first great experience with bootstrap.

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl")
 const app = express();
 const path = require("path");
+const { error } = require("console");
 
-app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
-app.get('/', function(req, res) { // serve main path as static file
+
+mongoose.connect(`mongodb://localhost:${process.env.PORT || 3000}/urlShortener`, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then( () => {
+    console.log("Sucsess");
+}).catch( (error) => {
+    console.log(error);
+}) 
+
+app.use(express.urlencoded({ extended: false} ))
+app.use('/', express.static(path.resolve('./dist')));               // serve main path as static dir
+
+app.get('/', async function(req, res) {     
+  const shortUrls = await ShortUrl.find()                          // serve main path as static file
   res.sendFile(path.resolve('dist\index.html'))
 });
+
+app.post("/shorturl", async (req, res) => {
+   await ShortUrl.create({ full: req.body.fullUrl })                     // create shortUrl
+   res.redirect("/")
+})
 
 
 app.listen(process.env.PORT || 3000);
