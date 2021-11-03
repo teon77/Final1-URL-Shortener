80d867057cdabb821a02141ad03f158eb8793267
Eithan Refaely <eithanrefaely@gmail.com>
2021-11-03 16:12:58 +0200

    Finished basic setup
    My front end will be developed on a different directory, and will be used as dist.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', function(req, res) { // serve main path as static file
+  res.sendFile(path.resolve('dist\index.html'))
+});
+
+
+app.listen(process.env.PORT || 3000);
